0208431ba20134d1bc1339e57e8d0f649690a97e
Fahad <fahad.fakky@hotmail.com>
2023-06-11 07:51:21 +0500

    homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:intagram_ui/login.dart';
 import 'package:intagram_ui/signup.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+      },
+    );
+  }
 }
 
 class HomePage extends StatelessWidget {
@@ -11,62 +27,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color(0xffFFFFFF),
-        body: Column(
+    return Scaffold(
+      backgroundColor: const Color(0xffFFFFFF),
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const SizedBox(
-              height: 50,
+              height: 400,
             ),
             const Image(
               image: AssetImage('assets/logo.png'),
+              height: 150,
+              width: 300,
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color(0xff3797EF),
-              ),
-              alignment: Alignment.center,
-              height: 45,
-              width: 307,
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  color: Color(0XffFFFFFF),
-                  fontFamily: 'Poppins-Black',
-                  fontSize: 14,
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff3797EF),
+                ),
+                alignment: Alignment.center,
+                height: 45,
+                width: 307,
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Color(0XffFFFFFF),
+                    fontFamily: 'Poppins-Black',
+                    fontSize: 14,
+                  ),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 100,
-              ),
-              child: Text.rich(
+            const SizedBox(
+              height: 200,
+            ),
+            const Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              child: const Text.rich(
                 TextSpan(
                   text: 'Don’t have an account? ',
-                  style: const TextStyle(
+                  style: TextStyle(
                     fontFamily: 'Poppins-Regular',
                     fontSize: 12,
                     color: Colors.grey,
                   ),
                   children: [
                     TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SignUpScreen(),
-                            ),
-                          );
-                        },
                       text: 'Sign Up.',
-                      style: const TextStyle(
+                      style: TextStyle(
                         fontFamily: 'Poppins-Black',
                         fontSize: 12,
                         color: Color(0xff262626),
